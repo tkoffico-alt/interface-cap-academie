6a5c14a0a679bde9df9d737d60fb980640a6aeec
tkoffico-alt <tkoffico@gmail.com>
2026-09-05 22:44:22 +0000

    Mise à jour

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // style.css pour forcer les appareils déjà installés à récupérer la
 // nouvelle version (sinon ils resteraient sur une version mise en cache).
 
-const CACHE_VERSION = 'edukatchat-shell-v43';
+const CACHE_VERSION = 'edukatchat-shell-v44';
 
 const FICHIERS_COQUILLE = [
     '/',
