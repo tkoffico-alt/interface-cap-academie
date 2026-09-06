9f7d8e2690b47a4c3d05ff2e8ed02f977fe60f16
tkoffico-alt <tkoffico@gmail.com>
2026-09-06 08:07:51 +0000

    Ajout Bibliotheque allemand et espagnol

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@
 // style.css pour forcer les appareils déjà installés à récupérer la
 // nouvelle version (sinon ils resteraient sur une version mise en cache).
 
-const CACHE_VERSION = 'edukatchat-shell-v45';
+const CACHE_VERSION = 'edukatchat-shell-v46';
 
 const FICHIERS_COQUILLE = [
     '/',
